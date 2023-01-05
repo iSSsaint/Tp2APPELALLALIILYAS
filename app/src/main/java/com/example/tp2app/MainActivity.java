4b9073d963c5796b56c8f30fc55dea8999447c11
ilyasaint <ilyaselallali3@gmail.com>
2023-01-05 19:14:09 +0100

    Commit partie 1 finished

--- app/src/main/java/com/example/tp2app/MainActivity.java
+++ app/src/main/java/com/example/tp2app/MainActivity.java
@@ -17,6 +17,10 @@ import java.net.URI;
 public class MainActivity extends AppCompatActivity {
     EditText phoneNumber;
     EditText webUrl;
+    public static int challenge1;
+    EditText chall1;
+    EditText chall2;
+    public static int challenge2;
     public static String num="";
 
 
@@ -26,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.phoneNumber=(EditText) findViewById(R.id.editTextTextPersonName2);
         this.webUrl=(EditText) findViewById(R.id.editTextWebsite);
+        this.chall1=findViewById(R.id.challenge1);
+        this.chall2=findViewById(R.id.challenge2);
     }
     public void onCall(View view){
         this.num=phoneNumber.getText().toString();
@@ -34,11 +40,16 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void onWorldClick(View view){
-        Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+        this.challenge1=Integer.parseInt(this.chall1.getText().toString());
+        this.challenge2=Integer.parseInt(this.chall2.getText().toString());
+        /*Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
         intent.setData(Uri.parse(this.webUrl.getText().toString()));
+        startActivity(intent);*/
+        Intent intent = new Intent(MainActivity.this,CheckActivity.class);
         startActivity(intent);
     }
     public void onPCClick(View view){
-
+        Intent intent = new Intent(MainActivity.this,CheckActivity.class);
+        startActivity(intent);
     }
 }
